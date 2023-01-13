f4ec9d77ea3148002ddfa478619677a80361481b
Nikita <nikitahay0993@gmail.com>
2023-01-13 02:52:09 +0000

    changes again

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -36,10 +36,9 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Blog"
+              description="My blog site created with NextJs and tailwind"
+              demoLink="#"
             />
           </Col>
 
@@ -47,10 +46,9 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="My Text Editor"
+              description="My Text editor i am creating using Golang"
+              demoLink="#"              
             />
           </Col>
         </Row>
